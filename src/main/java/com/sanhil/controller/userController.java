8f30d20193f5681c34503e6079cc7b860605b839
Sanhil143 <sanhilrai143@gmail.com>
2024-02-05 01:20:25 +0530

    add login with all validation

--- src/main/java/com/sanhil/controller/userController.java
+++ src/main/java/com/sanhil/controller/userController.java
@@ -21,7 +21,8 @@ public class userController {
 		if(user.getFirstName() == null || user.getLastName() == null || user.getEmail() == null || user.getPassword() == null || user.getRole() == null){
 			return ResponseEntity.badRequest().body("Please provide all fields");
 		}
-		if(UserRepository.existsByEmail(user.getEmail())){
+		userService existingUser = UserRepository.findByEmail(user.getEmail());
+		if(existingUser  != null){
 			return ResponseEntity.badRequest().body("Email is already Exist");
 		}
     user.setFirstName(user.getFirstName().toLowerCase().trim());
@@ -34,6 +35,23 @@ public class userController {
 		userService savedUser = UserRepository.save(user);
 		savedUser.setPassword("मैं नहीं बताऊँगा");
 		return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+	}
+
+	@PostMapping(value = "/login")
+	private ResponseEntity<?> loginUser(@RequestBody userService loginUser){
+		if(loginUser.getEmail() == null || loginUser.getPassword() == null){
+			return ResponseEntity.badRequest().body("Please provide email or password");
+		}
+		userService existingUser = UserRepository.findByEmail(loginUser.getEmail());
+		if(existingUser == null){
+			return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Please provide valid email");
+		}
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		if(!encoder.matches(loginUser.getPassword(),existingUser.getPassword())){
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Incorrect password");
+		}
+		existingUser.setPassword("मैं नहीं बताऊँगा");
+		return  ResponseEntity.status(HttpStatus.OK).body(existingUser);
 	}
 
 	@GetMapping(value = "/allUser")
